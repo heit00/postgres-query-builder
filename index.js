097641917efc35d91c4394c0266201a608496638
heit00 <heitorutyama@gmail.com>
2026-01-21 18:41:59 -0300

    added export to qb Class
    Export qb module for external use.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const qb = require('./lib/index');
-const x = new qb().select('x','y','z').from('a','b').where('T','>', qb.bind(2)).distinct().limit(2).offset(1).join('P',(join) => join.on('x','y.A').onOr('c', 'l').onGroup((x)=> x.add('z','=','p')));
-console.log(x.toInstruction());
+module.exports = qb
 
