60dce10916460aee45cbae41590d0f2d0c5091e1
heit00 <heitorutyama@gmail.com>
2026-02-15 16:05:48 -0300

    features: case clause

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const qb = require('./lib/index');
-const x = new qb().select('x','y','z').from('a','b').where('T','>', qb.bind(2)).distinct().limit(2).offset(1).join('P',(join) => join.on('x','y.A').onOr('c', 'l').onGroup((x)=> x.add('z','=','p')));
+const {CaseClause:cc, Query:qb, CaseClause, Column} = require('./lib/index');
+module.exports = qb;
+
+const y = new CaseClause();
+y.when('x','=','y', 2).when('u','z',4)
+const x = new qb().select(y).whereIn('2', ['1','2']).orWhereIn('k', ['1' ,'2' , qb.column('a.b')]).where(1,2).join('z','k', '=', y).from('k');
+
 console.log(x.toInstruction());
 
+/*
+ OUTPUT: 
+ {
+  template: 'DELETE FROM "a" WHERE ("x" = "z" AND ("x" = $1 OR "x" = "r"))',
+  values: [ 2 ]
+}
+*/
